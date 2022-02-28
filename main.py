8a954096864299210c1079e2cdec20d6c2359f91
Galal Mohammed <galalmoh2001@gmail.com>
2022-02-28 10:52:00 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+def BinarySearch (arr, firstIndex, lastIndex, num):
+    while arr[(firstIndex + lastIndex) // 2] != num:
+        if firstIndex != lastIndex:
+            if arr[(firstIndex + lastIndex) // 2] < num:
+                firstIndex = ((firstIndex + lastIndex) // 2) + 1
+            else:
+                lastIndex = ((firstIndex + lastIndex) // 2) - 1
+        else:
+            return -1
+    return (firstIndex + lastIndex) // 2
+print(BinarySearch([1, 3, 5, 7], 0, 3, 8))
