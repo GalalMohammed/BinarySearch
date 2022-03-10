678e65cf782a5927dcb3de4516c001b53d182396
Galal Mohammed <galalmoh2001@gmail.com>
2022-03-10 14:40:58 +0200

    bug fixing
    preventing lastIndex getting lower than firstIndex by modifying the condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def BinarySearch (arr, firstIndex, lastIndex, num):
     while arr[(firstIndex + lastIndex) // 2] != num:
-        if firstIndex != lastIndex:
+        if firstIndex < lastIndex:
             if arr[(firstIndex + lastIndex) // 2] < num:
                 firstIndex = ((firstIndex + lastIndex) // 2) + 1
             else:
@@ -8,4 +8,4 @@ def BinarySearch (arr, firstIndex, lastIndex, num):
         else:
             return -1
     return (firstIndex + lastIndex) // 2
-print(BinarySearch([1, 3, 5, 7], 0, 3, 8))
+print(BinarySearch([1, 3, 3, 4, 7], 0, 4, 8))
